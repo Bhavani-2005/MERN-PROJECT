e806f6bedc533d43c949ac339ac85d5dbe991680
Veena179 <your-email@example.com>
2026-05-16 13:27:04 +0530

    Fixed socket cors

--- backend/server.js
+++ backend/server.js
@@ -36,24 +36,39 @@ const adminRoutes =
 /* APP */
 
 const app = express();
+
 const server =
   http.createServer(app);
+
+/* ALLOWED ORIGINS */
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://smart-artisan-assistant.onrender.com",
+];
+
+/* SOCKET.IO */
 
 const io =
   new Server(server, {
 
     cors: {
+
       origin:
-        "http://localhost:5173",
+        allowedOrigins,
 
       methods: [
         "GET",
         "POST",
       ],
+
+      credentials: true,
     },
   });
 
-  app.use((req, res, next) => {
+/* SOCKET ACCESS */
+
+app.use((req, res, next) => {
 
   req.io = io;
 
@@ -62,7 +77,16 @@ const io =
 
 /* MIDDLEWARE */
 
-app.use(cors());
+app.use(
+
+  cors({
+
+    origin:
+      allowedOrigins,
+
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
@@ -122,9 +146,11 @@ app.use(
 
 app.get(
   /^(?!\/api).*/,
+
   (req, res) => {
 
     res.sendFile(
+
       path.join(
         __dirname,
         "../frontend/dist/index.html"
@@ -133,6 +159,7 @@ app.get(
   }
 );
 
+/* SOCKET CONNECTION */
 
 io.on(
   "connection",
@@ -156,32 +183,35 @@ io.on(
     );
   }
 );
+
 /* DATABASE */
 
 mongoose
   .connect(
     process.env.MONGO_URI
   )
+
   .then(() => {
 
     console.log(
       "MongoDB Connected"
     );
 
-  server.listen(
-  process.env.PORT || 5000,
+    server.listen(
 
-  () => {
+      process.env.PORT || 5000,
 
-    console.log(
-      `Server running on port ${
-        process.env.PORT || 5000
-      }`
+      () => {
+
+        console.log(
+          `Server running on port ${
+            process.env.PORT || 5000
+          }`
+        );
+      }
     );
-  }
-);
-
   })
+
   .catch((err) => {
 
     console.log(
